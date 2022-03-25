15b7566c70b23247440b5fc87834fd3b7c1ac38b
Chang Hoon <gelheart1011@gmail.com>
2022-03-25 10:26:08 +0900

    test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
-          
+          test
         </Switch>
       </div>
       <Footer />
