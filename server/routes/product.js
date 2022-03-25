261343d6d1ffc0db169817d7b3954f5214128313
Chang Hoon <gelheart1011@gmail.com>
2022-03-25 13:53:55 +0900

    route product image add

---
+++ server/routes/product.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image', (req, res)=>{
+
+});
+
+module.exports = router;
