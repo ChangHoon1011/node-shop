6c8ec160512107b527ac948f95f3dc26a8abfd67
Chang Hoon <gelheart1011@gmail.com>
2022-03-25 11:17:57 +0900

    add selector value

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -4,6 +4,15 @@ import {Typography, Button, Form, Input} from 'antd';
 const { Title } = Typography;
 const { TextArea } = Input;
 
+const Continents = [
+    {key:1, value: "Africa"},
+    {key:2, value: "Europe"},
+    {key:3, value: "Asia"},
+    {key:4, value: "North America"},
+    {key:5, value: "South America"},
+    {key:6, value: "Australia"},
+    {key:7, value: "Antarctica"}
+]
 
 function UploadProductPage() {
 
@@ -54,7 +63,9 @@ function UploadProductPage() {
                 <br/>
                 <br/>
                 <select onChange={continentChangeHandler} value={Continent }>
-                   
+                {Continents.map(item => (
+                        <option key={item.key} value={item.key}>{item.value}</option>
+                    ))}
                     <option></option>
                 </select>
                 <br/>
