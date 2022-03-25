4f16f43b8f9a30b88ea8ca7b98081141c7850d60
Chang Hoon <gelheart1011@gmail.com>
2022-03-25 11:13:59 +0900

    upload page design

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {Typography, Button, Form, Input} from 'antd';
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
 
 function UploadProductPage() {
+
+    const [Title, setTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Price, setPrice] = useState(0)
+    const [Continent, setContinent] = useState(1)
+    const [Images, setImages] = useState([])
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const descriptionChangeHandler = (event) =>{
+        setDescription(event.currentTarget.value)
+    }
+
+    const priceChandeHandler = (event) => {
+        setPrice(event.currentTarget.value)
+    }
+
+    const continentChangeHandler = (event) =>{
+        setContinent(event.currentTarget.value)
+    }
+
   return (
-    <div>UploadProductPage</div>
+    <div style={{maxWidth:'700px', margin: '2rem auto'}}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
+                <h2 level={2}>여행 상품 업로드</h2>
+            </div>
+            
+            <Form>
+
+            
+                
+                <br/>
+                <br/>
+                <label>이름</label>
+                <Input onChange={titleChangeHandler} value={Title} />
+                <br/>
+                <br/>
+                <label>설명</label>
+                <TextArea onChange={descriptionChangeHandler} value={Description} />
+                <br/>
+                <br/>
+                <label>가격($)</label>
+                <Input  type="number" onChange={priceChandeHandler} value={Price}/>
+                <br/>
+                <br/>
+                <select onChange={continentChangeHandler} value={Continent }>
+                   
+                    <option></option>
+                </select>
+                <br/>
+                <br/>
+                <Button>
+                    확인
+                </Button>
+            </Form>
+        </div>
   )
 }
 
