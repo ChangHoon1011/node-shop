0024338d1e6a3693b174310dfc6b1586d4593040
Chang Hoon <gelheart1011@gmail.com>
2022-04-01 09:41:25 +0900

    code prettierr

--- client/src/components/views/LandingPage/Sections/Datas.js
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -1,68 +1,65 @@
 const continents = [
-    {
-        "_id":1,
-        "name":"Africa"
-    },
-    {
-        "_id":2,
-        "name":"Europe"
-    },
-    {
-        "_id":3,
-        "name":"Asia"
-    },
-    {
-        "_id":4,
-        "name":"North America"
-    },
-    {
-        "_id":5,
-        "name":"South America"
-    },
-    {
-        "_id":6,
-        "name":"Australia"
-    },
-    {
-        "_id":7,
-        "name":"Antarctica"
-    },
-]
+  {
+    _id: 1,
+    name: "Africa",
+  },
+  {
+    _id: 2,
+    name: "Europe",
+  },
+  {
+    _id: 3,
+    name: "Asia",
+  },
+  {
+    _id: 4,
+    name: "North America",
+  },
+  {
+    _id: 5,
+    name: "South America",
+  },
+  {
+    _id: 6,
+    name: "Australia",
+  },
+  {
+    _id: 7,
+    name: "Antarctica",
+  },
+];
 
 const price = [
-    {
-        "_id":0,
-        "name": "Any",
-        "array":[]
-    },
-    {
-        "_id":1,
-        "name": "$0 to $199",
-        "array":[0,199]
-    },
-    {
-        "_id":2,
-        "name": "$200 to $249",
-        "array":[0,199]
-    },
-    {
-        "_id":3,
-        "name": "$250 to $279",
-        "array":[250,279]
-    },
-    {
-        "_id":4,
-        "name": "$280 to $299",
-        "array":[280,299]
-    },
-    {
-        "_id":5,
-        "name": "More than $300",
-        "array":[300,99999999999]
-    }
-]
+  {
+    _id: 0,
+    name: "Any",
+    array: [],
+  },
+  {
+    _id: 1,
+    name: "$0 to $199",
+    array: [0, 199],
+  },
+  {
+    _id: 2,
+    name: "$200 to $249",
+    array: [0, 199],
+  },
+  {
+    _id: 3,
+    name: "$250 to $279",
+    array: [250, 279],
+  },
+  {
+    _id: 4,
+    name: "$280 to $299",
+    array: [280, 299],
+  },
+  {
+    _id: 5,
+    name: "More than $300",
+    array: [300, 99999999999],
+  },
+];
 
-export {
-    continents,
-    price
-}
+export { continents, price };
