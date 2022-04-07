712a98cf403839b896ffe6382f298fd17a05bd64
Chang Hoon <gelheart1011@gmail.com>
2022-04-07 17:45:43 +0900

    Add to Cart 버튼만들기#2

--- server/routes/users.js
+++ server/routes/users.js
@@ -71,8 +71,47 @@ router.get('/logout', auth, (req, res) => {
 
 router.post('/addToCart', auth, (req, res) => {
   //먼저 User Collection에 해당하는 모든 정보를 가져오기
-  //가져온 정보에서 카트에다 넣으려 하는 상품이 있는지 확인하기
-  // 상품이 이미 있을때
-  // 상품이 이미 있지 않을때
+
+  User.findOne({ _id: req.user._id }, (err, userInfo) => {
+    //가져온 정보에서 카트에다 넣으려 하는 상품이 있는지 확인하기
+    let duplicate = false;
+    userInfo.cart.forEach((item) => {
+      if (item.id === req.body.productId) {
+        duplicate = true;
+      }
+    });
+
+    if (duplicate) {
+      // 상품이 이미 있을때
+      User.findOneAndUpdate(
+        { _id: req.user._id, 'cart.id': req.body.productId },
+        { $inc: { 'cart.$.quantity': 1 } },
+        { new: true },
+        (err, userInfo) => {
+          if (err) return res.status(200).json({ success: false, err });
+          res.status(200).send(userInfo.cart);
+        }
+      );
+    } else {
+      // 상품이 이미 있지 않을때
+      User.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: {
+            cart: {
+              id: req.body.productId,
+              quality: 1,
+              date: Date.now(),
+            },
+          },
+        },
+        { new: true },
+        (err, userInfo) => {
+          if (err) return res.status(400).json({ success: false, err });
+          res.status(200).send(userInfo.cart);
+        }
+      );
+    }
+  });
 });
 module.exports = router;
