d3934f9fff4335e671dcc80c798d20b08580a62b
Chang Hoon <gelheart1011@gmail.com>
2022-04-07 16:55:58 +0900

    product info 표만들기

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Button, Descriptions } from "antd";
 
-function ProductInfo() {
-  return <div>ProductInfo</div>;
+function ProductInfo(props) {
+  const ClickHandler = () => {};
+  return (
+    <div>
+      <Descriptions title="Product Info">
+        <Descriptions.Item label="Price">
+          {props.detail.price}
+        </Descriptions.Item>
+        <Descriptions.Item label="Sold">{props.detail.sold}</Descriptions.Item>
+        <Descriptions.Item label="View">{props.detail.views}</Descriptions.Item>
+        <Descriptions.Item label="Description">
+          {props.detail.Description}
+        </Descriptions.Item>
+      </Descriptions>
+
+      <br />
+      <br />
+      <br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button size="large" shape="round" type="danger" onClick={ClickHandler}>
+          Add to Cart
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default ProductInfo;
