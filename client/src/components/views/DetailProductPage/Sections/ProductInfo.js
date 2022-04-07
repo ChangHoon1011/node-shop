2dbef92a64e5e03a493730fa7e322cdaacbf947b
Chang Hoon <gelheart1011@gmail.com>
2022-04-07 17:19:22 +0900

    Add to Cart 버튼만들기#1

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { Button, Descriptions } from "antd";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../../_actions/user_actions";
 
 function ProductInfo(props) {
-  const ClickHandler = () => {};
+  const dispatch = useDispatch();
+
+  const clickHandler = () => {
+    // 필요한 정보를 카트에 넣어준다
+    dispatch(addToCart(props.detail._id));
+  };
+
   return (
     <div>
       <Descriptions title="Product Info">
@@ -20,7 +28,7 @@ function ProductInfo(props) {
       <br />
       <br />
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <Button size="large" shape="round" type="danger" onClick={ClickHandler}>
+        <Button size="large" shape="round" type="danger" onClick={clickHandler}>
           Add to Cart
         </Button>
       </div>
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,71 +1,78 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/User");
+const { User } = require('../models/User');
 
-const { auth } = require("../middleware/auth");
+const { auth } = require('../middleware/auth');
 
 //=================================
 //             User
 //=================================
 
-router.get("/auth", auth, (req, res) => {
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-        lastname: req.user.lastname,
-        role: req.user.role,
-        image: req.user.image,
-    });
+router.get('/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
 });
 
-router.post("/register", (req, res) => {
+router.post('/register', (req, res) => {
+  const user = new User(req.body);
 
-    const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
+router.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: 'Auth failed, email not found',
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: 'Wrong password' });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie('w_authExp', user.tokenExp);
+        res.cookie('w_auth', user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
         });
+      });
     });
+  });
 });
 
-router.post("/login", (req, res) => {
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user)
-            return res.json({
-                loginSuccess: false,
-                message: "Auth failed, email not found"
-            });
-
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "Wrong password" });
-
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                res.cookie("w_authExp", user.tokenExp);
-                res
-                    .cookie("w_auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true, userId: user._id
-                    });
-            });
-        });
-    });
+router.get('/logout', auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: '', tokenExp: '' },
+    (err, doc) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true,
+      });
+    }
+  );
 });
 
-router.get("/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).send({
-            success: true
-        });
-    });
+router.post('/addToCart', auth, (req, res) => {
+  //먼저 User Collection에 해당하는 모든 정보를 가져오기
+  //가져온 정보에서 카트에다 넣으려 하는 상품이 있는지 확인하기
+  // 상품이 이미 있을때
+  // 상품이 이미 있지 않을때
 });
-
 module.exports = router;
