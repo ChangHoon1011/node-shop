03db5cb5b033035883ab33704ecc84c2d95a0452
Chang Hoon <gelheart1011@gmail.com>
2022-04-08 10:58:16 +0900

    카트에 담긴 상품정보들을 데이터베이스에서 가져오기 #1

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getCartItems } from "../../../_actions/user_actions";
 
-function CartPage() {
+function CartPage(props) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let cartItems = [];
+
+    //리덕스 User state안에 cart안에 상품이 들어이쓴ㄴ지 확인
+    if (props.user.userData && props.user.userData) {
+      if (props.user.userdata.cart.length > 0) {
+        props.user.userData.cart.foreach((item) => {
+          cartItems.push(item.id);
+        });
+
+        dispatch(getCartItems(cartItems, props.user.userdat.cart));
+      }
+    }
+  }, []);
   return <div>CartPage</div>;
 }
 
